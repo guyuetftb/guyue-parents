1d63c182f1fb58cb0ec4b33dae0f19096eb89335
lipeng <lipeng02@missfresh.cn>
2023-04-21 17:48:49 +0800

    update

--- guyue-algorithm/src/main/java/com/gy/algorithm/basic/linkedlist/ReverseLinkedListTest.java
+++ guyue-algorithm/src/main/java/com/gy/algorithm/basic/linkedlist/ReverseLinkedListTest.java
@@ -8,102 +8,102 @@ package com.gy.algorithm.basic.linkedlist;
  */
 public class ReverseLinkedListTest {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		MyLinkedList<Integer> list1 = new MyLinkedList<>();
-		list1.add(1);
-		list1.add(3);
-		list1.add(5);
-		list1.add(7);
-		list1.add(9);
-		list1.add(2);
-		list1.add(4);
-		list1.add(6);
-		list1.add(8);
-		list1.add(10);
+        MyLinkedList<Integer> list1 = new MyLinkedList<>();
+        list1.add(1);
+        list1.add(3);
+        list1.add(5);
+        list1.add(7);
+        list1.add(9);
+        list1.add(2);
+        list1.add(4);
+        list1.add(6);
+        list1.add(8);
+        list1.add(10);
 
-		for (int i = 0; i < list1.size; i++) {
-			System.out.println(list1.get(i).getE());
-		}
+        for (int i = 0; i < list1.size; i++) {
+            System.out.println(list1.get(i).getE());
+        }
 
-		System.out.println("Swap Two linked Node ----------------------");
-		list1.reverse();
-		for (int i = 0; i < list1.size; i++) {
-			System.out.println(list1.get(i).getE());
-		}
+        System.out.println("Swap Two linked Node ----------------------");
+        list1.reverse();
+        for (int i = 0; i < list1.size; i++) {
+            System.out.println(list1.get(i).getE());
+        }
 
-	}
+    }
 
 
-	private static class MyLinkedList<E> {
+    private static class MyLinkedList<E> {
 
-		private LinkedNode preHead;
-		private int size;
+        private LinkedNode preHead;
+        private int size;
 
-		public MyLinkedList() {
-			preHead = new LinkedNode();
-			size = 0;
-		}
+        public MyLinkedList() {
+            preHead = new LinkedNode();
+            size = 0;
+        }
 
-		public void reverse() {
-			preHead.next = reverse(preHead.next);
-		}
+        public void reverse() {
+            preHead.next = reverse(preHead.next);
+        }
 
-		public LinkedNode reverse(LinkedNode head) {
-			// TODO 1 -> 2 -> 3 -> 4 -> 5
-			// 		把整个链表反转, 但是 preHead不反转
-			LinkedNode cur = head;
-			LinkedNode pre = null;
-			while (cur != null) {
+        public LinkedNode reverse(LinkedNode head) {
+            // TODO 1 -> 2 -> 3 -> 4 -> 5
+            // 		把整个链表反转, 但是 preHead不反转
+            LinkedNode cur = head;
+            LinkedNode pre = null;
+            while (cur != null) {
 
-				// 1. 把 cur.next 赋给 curNext, 暂存.
-				LinkedNode curNext = cur.next;
+                // 1. 把 cur.next 赋给 curNext, 暂存.
+                LinkedNode curNext = cur.next;
 
-				// cur 反指, 把 pre -> cur 变成  pre <- cur.next.
-				cur.next = pre;
+                // cur 反指, 把 pre -> cur 变成  pre <- cur.next.
+                cur.next = pre;
 
-				// pre 赋为 cur, pre 向前递进一个元素.
-				pre = cur;
+                // pre 赋为 cur, pre 向前递进一个元素.
+                pre = cur;
 
-				// cur 赋为 下一个节点 curNext 2; cur 向前递进一个元素.
-				cur = curNext;
-			}
+                // cur 赋为 下一个节点 curNext 2; cur 向前递进一个元素.
+                cur = curNext;
+            }
 
-			return pre;
-		}
+            return pre;
+        }
 
-		public int getSize() {
-			return size;
-		}
+        public int getSize() {
+            return size;
+        }
 
-		public void add(E e) {
-			add(size, e);
-		}
+        public void add(E e) {
+            add(size, e);
+        }
 
-		public LinkedNode get(int index) {
-			if (0 > index || index >= size) {
-				throw new IllegalArgumentException("Get failed. Illegal index.");
-			}
+        public LinkedNode get(int index) {
+            if (0 > index || index >= size) {
+                throw new IllegalArgumentException("Get failed. Illegal index.");
+            }
 
-			LinkedNode cur = preHead.next;
-			for (int i = 0; i < index; i++) {
-				cur = cur.next;
-			}
+            LinkedNode cur = preHead.next;
+            for (int i = 0; i < index; i++) {
+                cur = cur.next;
+            }
 
-			return cur;
-		}
+            return cur;
+        }
 
-		public void add(int index, E e) {
-			if (index < 0 || index > size) {
-				throw new IllegalArgumentException("Add failed. Illegal index.");
-			}
+        public void add(int index, E e) {
+            if (index < 0 || index > size) {
+                throw new IllegalArgumentException("Add failed. Illegal index.");
+            }
 
-			LinkedNode prev = preHead;
-			for (int i = 0; i < index; i++) {
-				prev = prev.next;
-			}
-			prev.next = new LinkedNode(e, prev.next);
-			size++;
-		}
-	}
+            LinkedNode prev = preHead;
+            for (int i = 0; i < index; i++) {
+                prev = prev.next;
+            }
+            prev.next = new LinkedNode(e, prev.next);
+            size++;
+        }
+    }
 }
---
+++ guyue-algorithm/src/main/java/com/gy/algorithm/basic/stack/ValidParentheses20.java
@@ -0,0 +1,44 @@
+package com.gy.algorithm.basic.stack;
+
+import java.util.Stack;
+
+public class ValidParentheses20 {
+    public static void main(String[] args) {
+
+    }
+
+    // 左括号 压栈
+    // 右括号 弹栈, 比较是否匹配
+    public boolean isValid(String s) {
+        if (null == s || s.length() % 2 == 1) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if ('(' == currentChar) {
+                stack.push(currentChar);
+            } else if ('[' == currentChar) {
+                stack.push(currentChar);
+            } else if ('{' == currentChar) {
+                stack.push(currentChar);
+            } else {
+                // 栈为空, 返回false
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character topChar = stack.peek();
+                if ((')' == currentChar && topChar == '(')
+                        || (']' == currentChar && topChar == '[')
+                        || ('}' == currentChar && topChar == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
